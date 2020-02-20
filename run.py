b807e91502ab249f9123bbc8e819bea5dbef9e65
valentin <valentin.lefranc@see-d.fr>
2020-02-20 10:48:14 +0100

    Le Skiff

--- run.py
+++ run.py
@@ -12,7 +12,7 @@ app.jinja_env.cache = {}
 
 if socket.gethostname() in ['CFT-AZURE-LX004','CFT-AZURE-LX005']:
     if __name__ == '__main__':
-        app.run(debug=True,host= '0.0.0.0', port = 5000)
+        app.run(debug=True,host= '0.0.0.0', port = 80)
 else:
     if __name__ == '__main__':
         app.run(debug=True, port = 5000)
